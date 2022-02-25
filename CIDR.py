d6255e8d7ea2b4d8df7b94549083cd14dad35d7d
dholl205 <daelanhollingsworth205@gmail.com>
2022-02-25 11:17:40 -0600

    Final Commit

---
+++ CIDR.py
@@ -0,0 +1,61 @@
+import requests, csv, json
+
+
+apple = {
+  "cidr": "",
+  "depthLimit": 1, 
+  "bogonsOnly": "false", 
+  "localityLanguage": "en",
+  "key": '66364ee2827341bf8c95821153c21056'
+}
+
+final = []
+
+
+def use(ipAddress):
+  apple["cidr"] = ipAddress + "/32"
+  r = requests.get('https://api.bigdatacloud.net/data/network-by-cidr', params=apple)
+  appending(r.json())
+  
+
+def appending(data):
+  dataset = []
+  dataset.clear()
+  dataset.append(data["cidr"])
+  dataset.append(data["network"]["carriers"][0]["asn"])
+  dataset.append(data["network"]["subnets"][0]["organisation"])
+  final.append(dataset)
+
+def readFile():
+  data = []
+  with open('ip_test_input.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+      data.append(row[0].replace(" ", ""))
+    data.remove(data[0])
+    return data
+
+
+
+def writeFile():
+  listings = []
+  for i in final:
+    newdict = {
+      "CIDR": i[0],
+      "ASN": i[1],
+      "Organisation": i[2]
+    }
+    listings.append(newdict)
+  with open('test.json', "w+") as file:
+    json.dump(listings, file)
+
+
+def main():
+  listings = readFile()
+  for ip in listings:
+    use(ip)
+  writeFile()
+  print(final)
+
+
+main()
